75caab7f4e1e33690a4d6f18322e08d22e955fea
Brayan vera <tecnpi2@gmail.com>
2021-10-16 12:53:22 -0500

    Chepo

--- components/ViewOffice/AboutViews.tsx
+++ components/ViewOffice/AboutViews.tsx
@@ -5,6 +5,7 @@ import { MdCoffeeMaker } from 'react-icons/md'
 import GrLocation from 'react-icons/gr'
 import { GoLocation } from 'react-icons/go'
 import ListItemIcon from '@mui/material/ListItemIcon';
+import OfficeMap from '../Maps/OfficeMap'
 interface AboutProps {
   office: any[]
 }
@@ -12,18 +13,25 @@ interface AboutProps {
 const AboutViews: FC<AboutProps> = (props) => {
   const office = props.office
   return (
-    <div style = {{width : '90%', marginLeft : '50px'}}>
+    <div style={{ width: '90%', marginLeft: '50px' }}>
       <h1 className={style.titleAbout}>
         {office.name}
       </h1>
 
-      <div className = {style.direction}>
-        <GoLocation  color = '#E64980'/>
+      <div className={style.direction}>
+        <GoLocation color='#E64980' />
         {office.address.formatted_address}
       </div>
-      
-      <div className= {style.description}>
+
+      <div className={style.description}>
         <p>{office.description}</p>
+      </div>
+
+      <div>
+        <h1 className={style.titleAbout}>
+          Ubicacion de la oficina
+        </h1>
+        <OfficeMap office={office} />
       </div>
     </div >
 
