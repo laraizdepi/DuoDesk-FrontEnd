ffb4af9c3b0757852f70e01aefe5f486bc53b24f
Brayan vera <tecnpi2@gmail.com>
2021-10-18 15:47:00 -0500

    Chepo Tabs

---
+++ components/ViewOffice/tabsSpace.tsx
@@ -0,0 +1,126 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import CardSearchBaseTest from './CardSearchBaseTest'
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 4 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+interface Offices {
+    id: string,
+    name: string,
+    description: string,
+    host: any,
+    isActive: boolean,
+    generalAmenities: string[]
+    spaces: {
+        nameSpace: string,
+        typeSpace: string,
+        capacitySpace: number,
+        availableSpace: number,
+        hourPrice: number,
+        dayPrice: number,
+        weekPrice: number,
+        monthPrice: number,
+        nameAmenities: string[],
+        imagesUrls: string[],
+        booking?: any
+    }[],
+    address: any,
+    scores?: {
+        averageScore: number,
+        reviews: any
+    },
+    days: [{
+        day: string,
+        isAvailable: boolean,
+        startHour?: string,
+        endHour?: string
+    }],
+    notifications: string[],
+    official: string[],
+    openDate: string
+  }
+
+const TabsSpace:React.FC<{office : Offices}> = (props) => {
+    const findTypeSpaces = (spaces:any[], typeSpace:string) =>{
+        const allSpaces: any[] = []
+        for (const space of spaces){
+          // console.log(space);
+          if (space.typeSpace == typeSpace){
+            console.log('yes');
+            allSpaces.push(space)
+            console.log('allSpaces', allSpaces);
+          }else{
+            console.log('not'); 
+          }
+          console.log(allSpaces);
+          
+        }
+        return allSpaces
+      }
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                    <Tab label="Escritorio dedicado" {...a11yProps(0)} />
+                    <Tab label="Oficina Privada" {...a11yProps(1)} />
+                    <Tab label="Sala de conferencias" {...a11yProps(2)} />
+                    <Tab label="Espacio abierto" {...a11yProps(3)} />
+                </Tabs>
+            </Box>
+            <TabPanel value={value} index={0}>
+                <CardSearchBaseTest/>
+                
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                Item Three
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+                Item Four
+            </TabPanel>
+        </Box>
+    );
+}
+
+
+export default TabsSpace
